c7286c4e67cacb5f072cf41a13ea730af46d30e0
Falade Oluwafemi <38985207+femi89@users.noreply.github.com>
2020-02-12 21:40:51 +0100

    Update db_data.php

--- private/db_data.php
+++ private/db_data.php
@@ -3,7 +3,7 @@
  * please replace it with the correct database information from
  * your server.
  * */
-define('DB_HOST', 'localhost:3306');
+define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', 'Falade000.');
+define('DB_PASS', 'Password');
 define('DB_NAME', 'oil_gas');
